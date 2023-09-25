664a3d48174a00f82e42ac67b4f1d550d583a18f
adfxxx <vdoyanova@mail.ru>
2023-09-25 20:34:28 +0300

    1_1

---
+++ 1_1/1_1.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+int flag_check(char *flag){
+    char flags [12][3] = {"-h", "/h", "-p", "/p", "-s", "/s", "-e", "/e", "-a", "/a", "-f", "/f"};
+    
+    if (strlen(flag) != 2){
+        return 0;
+    }
+
+    for (int i = 0; i < 12; ++i){
+        if (strcmp(flag, flags[i]) == 0){
+            return 1;
+        }
+    }
+
+    return 0;
+}
+///**************///
+int number_check(char *number){
+    if (strlen(number) == 1){
+        if (*number == '-'){
+            return 0;
+    }
+    }
+    if(sscanf (number, "%d", &number)){
+        return 1;
+    }
+    else{return 0;}
+}
+///**************///
+int flag_h(int number){
+    int status = 0;
+    if (number <= 0 || number > 100) {
+        printf ("Число должно быть больше 0 и меньше или равно 100.\n");
+    }
+    else{
+        for (int i = number; i <= 100; ++i){
+            if (i % number == 0){
+                printf("%d ", i);
+                status = 1;
+            }
+        }
+        if (status == 0){ printf("В пределах 100 нет чисел, кратных введенному.");} 
+    }
+    
+}
+///**************///
+int flag_p(int number){
+    if (number >= 2){
+        for (int i = 2; i < number; ++i ){
+            if (number % i == 0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+    else if (number == 1){
+        return 0;
+    }
+    else { return -1; }
+}
+///**************///
+int flag_s(char *number){
+    int position = 0;
+    if (number[0] == '-'){
+        position += 1;
+    }
+    if (number[position] == '0'){
+        while (number[position] == '0'){
+            position += 1;
+        }
+    }
+    for (int i = position; i < strlen(number); ++i){
+        printf("%c ", number[i]);
+    }
+}
+///**************///
+int calculations_for_e(int num, int pow){
+    long long int total = num;
+    for (int i = 1; i < pow; ++i){
+        total *= num;
+    }
+    return total;
+}
+
+int flag_e(int number){
+    if (number <= 0 || number > 10) {
+        printf ("Число должно быть от 1 до 10");
+    }
+    else {
+        printf("|  Степень  |    Значение   |\n");
+        printf("|-----------|---------------|\n");
+        for (int i = 1; i <= number; ++i){
+            printf ("|%6d     |", i);
+            for (int j = 1; j <= 10; ++j){
+                printf("%11lli    |\n", calculations_for_e(j, i));
+                if (j != 10){
+                    printf("|           |");
+                }
+            }
+            printf("|___________|_______________|\n");
+        }
+    }
+    
+}
+///**************///
+int flag_a(int number){
+    int answer = 0;
+    if (number <= 0){
+        printf("Число должно быть больше 0");
+    }
+    else {
+        printf("%d", (number*(number+1))/2);
+    }
+}
+///**************///
+int flag_f(unsigned long long number){
+    unsigned long long total = 1;
+    for (int i = 1; i <= number; ++i){
+        total *= i;
+    }
+    printf("%llu", total);
+}
+///**************///
+int main(int argc, char *argv[]){
+    setlocale (LC_ALL, "rus");
+
+    if (argc != 3){
+        printf ("Необходимо ввести два аргумента.");
+        return 0;
+    }
+
+    if (!number_check(argv[1])){
+        printf ("Введено неправильное число. Число не должно содержать лишних символов в своей записи.\n");
+        return 0;
+    }
+
+    if (!flag_check(argv[2])){
+        printf ("Введен неправильный флаг.\n");
+        return 0;
+    }
+
+    int number = atoi(argv[1]);
+    char flag = argv[2][1];
+
+    switch (flag){
+        case 'h':
+            flag_h(number);
+            break;
+        case 'p':
+            if(flag_p(number) == 1){
+                printf("Число простое");
+            }
+            else if (!flag_p(number)){
+                printf("Число составное");
+            }
+            else {printf ("Число должно быть больше 0.\n");}
+            break;
+        case 's':
+            flag_s(argv[1]);
+            break;
+        case 'e':
+            flag_e(number);
+            break;
+        case 'a':
+            flag_a(number);
+        case 'f':
+            if (number < 0){
+                printf("Число должно быть больше или равно 0");
+            }
+            else if (number == 0){
+                printf("%d", 1);
+            }
+            else {flag_f(number);}
+    }
+
+    return 0;
+}
